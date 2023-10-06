afcb15b95e328e6074896906be0ca617b8fabff9
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2023-10-05 21:59:04 -0300

    Menu de sistema

---
+++ Java_Poo/CadastroPessoa05-10/src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Bem vindo ao Sistema de cadastro de Pessoa Fisica e Juridica!!"); // sout + tab
+        JOptionPane.showMessageDialog(null,"Bem vindo ao Sistema de cadastro de Pessoa Fisica e Juridica!!");
+        Scanner scanner = new Scanner(System.in); // caso nao importar, ctrl + espaco em cima do Scanner
+
+        do {
+            System.out.println();
+        }while();
+
+    }
+}
