5c314b2a6ccc592101cf6b4f84cbdd466550e39b
jeffersonsouzasilva <jefferson.souzasilva777@gmail.com>
2023-11-06 21:53:50 -0300

    Criptografia

--- Webservice - API - CRUD  24-10/API_VsConnect/apivsconnect/src/main/java/com/senai/apivsconnect/repositories/UsuarioRepository.java
+++ Webservice - API - CRUD  24-10/API_VsConnect/apivsconnect/src/main/java/com/senai/apivsconnect/repositories/UsuarioRepository.java
@@ -2,13 +2,14 @@ package com.senai.apivsconnect.repositories;
 
 import com.senai.apivsconnect.models.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository /*adicionado*/
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, UUID> {
-    UsuarioModel findByEmail(String email);
+    UserDetails findByEmail(String email);
 
 
 }
